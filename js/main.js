e9aeea4d6b2abbf4e058b0c829110a9ea88a7ffc
Dmytro Slutskyi <58708977+yuppi711699@users.noreply.github.com>
2020-08-06 12:27:23 +0300

    first

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// const { type } = require("os")
+
+$(function(){
+    $('.photo__slider').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow_left.jpg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/arrow_rigth.jpg" alt=""></button>'
+
+    })
+
+});
